b94d8b40ea578764177010c5c1445ad5c9ef33c2
Vedant <vedant@localhost.localdomain>
2021-06-17 18:55:53 +0530

    Styled components

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from "react";
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import styled from 'styled-components/native';
+
+const CenteredView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const H1 = styled.Text`
+  font-size: 48px;
+  font-weight: bold;
+`;
+
+const P = styled.Text`
+  margin: 24px 0;
+  font-size: 18px;
+`;
 
 const Main = () => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Hello World</Text>
-        </View>
+        <CenteredView>
+            <H1>Hello World!</H1>
+            <P>This is my App.</P>
+            <Image source={require('./assets/images/hello-world.jpg')} />
+        </CenteredView>
     );
 }
 
