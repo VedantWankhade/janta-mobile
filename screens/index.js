a5257553f23ddeef74091f0db571ea22a3d2117c
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-18 14:49:19 +0530

    Stack navigation skeleton

--- screens/index.js
+++ screens/index.js
@@ -1,26 +1,43 @@
 import React from "react";
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from "react-navigation-stack";
 
 import Feed from "./feed";
 import Favorites from "./favorites";
 import MyNotes from "./mynotes";
+import NoteScreen from "./note";
+
+const FeedStack = createStackNavigator({
+    Feed,
+    Note: NoteScreen
+});
+
+const MyStack = createStackNavigator({
+    MyNotes,
+    Note: NoteScreen
+});
+
+const FavStack = createStackNavigator({
+   Favorites,
+   Note: NoteScreen
+});
 
 const TabNavigator = createBottomTabNavigator({
     FeedScreen: {
-        screen: Feed,
+        screen: FeedStack,
         navigationOptions: {
             tabBarLabel: 'Feed'
         }
     },
     MyNoteScreen: {
-        screen: MyNotes,
+        screen: MyStack,
         navigationOptions: {
             tabBarLabel: 'My Notes'
         }
     },
     FavoriteScreen: {
-        screen: Favorites,
+        screen: FavStack,
         navigationOptions: {
             tabBarLabel: 'Favorites'
         }
