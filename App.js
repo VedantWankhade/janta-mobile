cb70995b92aaf0806155fbc6edf4e534b66a0c62
VedantWankhade <vedantwankhade17@gmail.com>
2021-08-05 18:39:57 +0530

    Use to ApolloClient to connect to API

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import getEnvVars from "./config";
 
 import Screens from './screens';
 
+const { API_URI } = getEnvVars();
+const uri = API_URI;
+const cache = new InMemoryCache();
+
+// configure apollo client
+const client = new ApolloClient({
+    uri,
+    cache
+})
+console.log(uri)
+
 const Main = () => {
-    return (<Screens />);
+    return (
+        <ApolloProvider client={client}>
+            <Screens />
+        </ApolloProvider>
+    );
 }
 
 export default Main;
