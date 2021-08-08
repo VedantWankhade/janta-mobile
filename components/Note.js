a9f31672c2b13b8b227d751a23ca01522c069642
VedantWankhade <vedantwankhade17@gmail.com>
2021-08-08 18:31:22 +0530

    App markdown preview for note

--- components/Note.js
+++ components/Note.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Text, ScrollView } from "react-native";
 import styled from "styled-components/native";
+import Markdown from 'react-native-markdown-renderer';
 
 const NoteView = styled.ScrollView`
   padding: 10px;
 `;
 
 const Note = props => {
+    const note = props.note;
+
     return (
         <NoteView>
-            <Text>{props.note.content}</Text>
+            {/*remember to put formated date here*/}
+            <Text>Note by {note.author.username} / Published {' '}</Text>
+            <Markdown>{note.content}</Markdown>
         </NoteView>
     )
 }
--- screens/note.js
+++ screens/note.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { useQuery, gql } from "@apollo/client";
+
+import Note from "../components/Note";
+
+const GET_NOTE = gql`
+    query note($id: ID!) {
+        note(id: $id) {
+            id
+            createdAt
+            content
+            favoriteCount
+            author {
+                username
+                id
+                avatar
+            }
+        }
+    }
+`;
 
 const NoteScreen = props => {
     const id = props.navigation.getParam('id');
+    const { loading, error, data } = useQuery(GET_NOTE, { variables: { id }});
 
-    return (
-        <View style={{padding: 10}}>
-            <Text>This is a note {id}</Text>
-        </View>
-    );
+    if (loading) return <Text>Loading...</Text>;
+    if (error) return <Text>Error occcured</Text>;
+
+    return <Note note={data.note} />;
 }
 
 export default NoteScreen;
